1051524e6fa5256dbf057c9fc292e4a11ba5bf0a
CardinalGrayRan <11587722+amerilouis@user.noreply.gitee.com>
2023-05-07 00:33:03 +0800

    edit port

--- frontend/src/components/Context/UrlSource.ts
+++ frontend/src/components/Context/UrlSource.ts
@@ -2,9 +2,9 @@ import {Navigate} from "react-router-dom";
 
 export default function GetUrl(type){
 
-    let url0 = 'http://127.0.0.1:5000/api/'
+    let url0 = 'http://127.0.0.1:8080/api/'
     let url = 'http://lee666.sv1.k9s.run:2271/api/'
     let urlb = '/api/'
 
-    return url0 + type
+    return urlb + type
 }
