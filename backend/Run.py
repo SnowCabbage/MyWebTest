3f3102fd94db5919bd1877399ea794c820cb3cc0
CardinalGrayRan <11587722+amerilouis@user.noreply.gitee.com>
2023-05-06 11:13:08 +0800

    update

---
+++ backend/Run.py
@@ -0,0 +1,19 @@
+from Flaskr import app
+from waitress import serve
+import multiprocessing
+
+
+# def serverOn():
+#     # name = multiprocessing.current_process().name
+#     name = multiprocessing.current_process().name
+#     print("Starting %s " % name)
+#     serve(app, host='127.0.0.1', port=5000)
+#     print("Exiting %s " % name)
+
+
+if __name__ == '__main__':
+    # background_process = multiprocessing.Process(name='background_process', target=serverOn)
+    # background_process.daemon = True
+    # background_process.start()
+    # print("This is output")
+    serve(app, host='127.0.0.1', port=5000)
--- backend/debug.py
+++
@@ -1,6 +0,0 @@
-from Flaskr import app
-from waitress import serve
-
-if __name__ == '__main__':
-    # app.secret_key = 'dev'
-    serve(app, host='127.0.0.1', port=5000)
