c8a4fd33c41916bb53ad98d6696d23ef7816b81d
CardinalGrayRan <11587722+amerilouis@user.noreply.gitee.com>
2023-05-07 13:48:22 +0800

    update mysql setting

--- backend/Flaskr/config/production_config.py
+++ backend/Flaskr/config/production_config.py
@@ -4,6 +4,6 @@ JSON_AS_ASCII = True
 DEBUG = False
 ENV = 'production'
 SECRET_KEY = 'SnowCabbage'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:kissyou@127.0.0.1:3306/test'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:kissyou@127.0.0.1:3306/test'
 JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30)
 PROPAGATE_EXCEPTIONS = True
